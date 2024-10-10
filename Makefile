931d48714455844cbb7eb4dd2366919bd9ae9be4
NoAngel <155915830+NopAngel@users.noreply.github.com>
2024-10-10 11:06:58 -0400

    support: cmake in linux and windows.
    This is a Support in Linux.🔩

---
+++ Makefile
@@ -0,0 +1,11 @@
+.PHONY: clean run
+.SILENT:
+
+cube.o: cube.c
+	gcc -o $@ $< -lm
+
+run: cube.o
+	./$<
+
+clean:
+	rm -rf cube.o
---
+++ out/build/x64-Debug/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Angel Nieto/Desktop/Workscapes/cube-donut
+# Build directory: C:/Users/Angel Nieto/Desktop/Workscapes/cube-donut/out/build/x64-Debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
